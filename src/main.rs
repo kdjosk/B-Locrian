2bd05e229cfdadf0b8569d4704f400a233a069f5
Krzysztof Jóskowiak <krzysztof.josk@gmail.com>
2023-05-07 22:08:34 +0200

    Add polishing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
 use b_locrian::parser::Parser;
 use b_locrian::pretty_print::AstPrinter;
-use b_locrian::scanner::{Scanner, TokenType};
 use std::env;
 use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if let Ok(src) = fs::read_to_string(&args[1]) {
-        println!("{}", src);
-        let mut lex = Scanner::new(&src);
-        loop {
-            match lex.scan_token() {
-                Ok(t) => {
-                    println!("{:?}", t);
-                    if t.ty == TokenType::Eof {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    println!("{:?}", e);
-                    break;
-                }
-            }
-        }
-
         let mut parser = Parser::new(&src);
         let tree = parser.parse();
         AstPrinter::new(&tree).print_tree();
